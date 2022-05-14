fbd33bbae785232415f1de974ab2901a716c5faf
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-14 12:13:54 +0600

    Updating

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import turtle
 from turtle import Turtle, Screen
 import pandas as pd
 
 
 screen = Screen()
+screen.setup(width=700, height=700)
 screen.title("Bangladesh district game")
 
 # Lodaing up the image of Bangladesh
-image =
+image = "b.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+screen.exitonclick()
